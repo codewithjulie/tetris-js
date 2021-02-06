b7f6cff8ed10a36f8b80cbba08bddacf0707b5bb
codewithjulie <codewithjulie@gmail.com>
2021-02-06 09:15:04 -0800

    created gameboard

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('#gameboard');
+const ctx = canvas.getContext('2d');
+
+const canvasWidth = 400;
+const canvasHeight = 800;
+
+ctx.canvas.width = canvasWidth;
+ctx.canvas.height = canvasHeight;
